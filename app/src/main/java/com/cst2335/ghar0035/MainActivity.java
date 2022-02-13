38e2ff0fc0f0ad25c3e329229b4671f5dc36c999
ghar0035 <ghar0035@algonquinlive.com>
2022-02-13 17:17:05 -0500

    Finished lab 3

--- app/src/main/java/com/cst2335/ghar0035/MainActivity.java
+++ app/src/main/java/com/cst2335/ghar0035/MainActivity.java
@@ -2,107 +2,61 @@ package com.cst2335.ghar0035;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "Main_Activity";
+    public final static String USER_DETAIL = "UserDetails";
+    // View variables
+    private Button loginButton;
+    private EditText inputEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        loginButton = findViewById(R.id.btnLogin);
+        inputEmail = findViewById(R.id.emailAddress);
+
+        // get from shared value file the email address and set the email field text to that value
+        SharedPreferences prefs = getSharedPreferences( USER_DETAIL , Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("emailAddress", "");
+        inputEmail.setText(emailAddress);
+
+
+        loginButton.setOnClickListener(click -> {
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+            goToProfile.putExtra("EMAIL",inputEmail.getText().toString() );
+            startActivity(goToProfile);
+        });
+
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
+        // Save the email input text into shared preferences
+        SharedPreferences prefs = getSharedPreferences( USER_DETAIL , Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString("emailAddress",inputEmail.getText().toString());
+        edit.commit();
     }
-
-
 }
-
-/*
-        Resources res = getResources();
-        String toast_message= res.getString(R.string.toast_message);
-        String snackbar_on= res.getString(R.string.snackbar_on);
-        String snackbar_off= res.getString(R.string.snackbar_off);
-        String Undo = res.getString(R.string.Undo);
-
-        Button bttn = findViewById(R.id.button2);
-        bttn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText( MainActivity.this,toast_message,Toast.LENGTH_LONG).show();
-            }
-        });
-
-        Switch sw = findViewById(R.id.switch1);
-
-        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Snackbar snackbar = Snackbar
-                            .make(findViewById(R.id.layoutRoot), snackbar_on, Snackbar.LENGTH_LONG)
-                            .setAction(Undo, new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    String status="";
-                                    Snackbar snackbar1 = Snackbar.make(findViewById(R.id.layoutRoot), snackbar_off+ status, Snackbar.LENGTH_SHORT);
-                                    snackbar1.show();
-                                }
-                            });
-                    snackbar.show();
-                }else {
-                    Snackbar snackbar = Snackbar
-                            .make(findViewById(R.id.layoutRoot), snackbar_off, Snackbar.LENGTH_LONG)
-                            .setAction(Undo, new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    String status="";
-                                    Snackbar snackbar1 = Snackbar.make(findViewById(R.id.layoutRoot), snackbar_on+ status, Snackbar.LENGTH_SHORT);
-                                    snackbar1.show();
-                                }
-                            });
-                    snackbar.show();
-                }
-            }
-        });   */
-        /*
-        Resources resources = getResources();
-        String toast_message= res.getString(R.string.toast_message);
-        String snackbar_on= res.getString(R.string.snackbar_on);
-        String snackbar_off= res.getString(R.string.snackbar_off);
-
-        Button bttn = findViewById(R.id.button2);
-        bttn.setOnClickListener( v -> {
-                Toast.makeText(MainActivity.this, toast_message, Toast.LENGTH_LONG).show();
-        });
-
-        Switch sch = findViewById(R.id.switch1);
-        sch.setOnCheckedChangeListener(CompoundButton cb, boolean b){
-            if (b == true) {
-                Snackbar.make(findViewById(R.id.layoutRoot), snackbar_on, Snackbar.LENGTH_SHORT);
-                Snackbar.setAction( “Undo”, click -> cb.setChecked(!b);
-                Snackbar.show();
-            }else(b == false ) {
-                Snackbar.make(findViewById(R.id.layoutRoot), R.string.snackbar_off, Snackbar.LENGTH_SHORT);
-                Snackbar.setAction( “Undo”, click -> cb.setChecked(!b);
-                Snackbar.show();
-        });
-
-*/
-/*
-        mCheckBox.setOnCheckedChangeListener((view, isChecked) -> mCrime.setSolved(isChecked));
-
-        mCheckBox.setOnCheckedChangeListener(buttonView, isChecked -> mCrime.setSolved(isChecked))
- */
-
 
--- app/src/main/java/com/cst2335/ghar0035/ProfileActivity.java
+++ app/src/main/java/com/cst2335/ghar0035/ProfileActivity.java
@@ -1,6 +1,13 @@
 package com.cst2335.ghar0035;
 
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResult;
+
 
 import android.app.Activity;
 import android.app.Instrumentation;
@@ -10,49 +17,94 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     public static final String TAG = "PROFILE_ACTIVITY";
+
+    // ImageView imgView;
+    ImageButton imageButton;
+    EditText emailField;
+
+     // Activity launcher
+    ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult()
+            ,new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                   if (result.getResultCode() == Activity.RESULT_OK) {
+                       Intent data = result.getData();
+                       Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
+                       // imgView.setImageBitmap(imgbitmap);
+                    } else if(result.getResultCode() == Activity.RESULT_CANCELED) {
+                       Log.i(TAG, "User refused to capture a picture.");
+                   }
+                }
+            });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        Log.e(TAG, "In function: onCreate ");  /*add logging information calls to all of the functions*/
+        Log.e(TAG, "In function: onCreate ");
 
 
-        ImageButton imageButton = findViewById(R.id.imageButton2);
-    /*    imageButton.setOnClickListener(click ->  {
+        imageButton = findViewById(R.id.btnUpload);
+        emailField = findViewById(R.id.yourEmail);
 
-            ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
-                    new ActivityResultContracts.StartActivityForResult()
-                    ,new ActivityResultCallback<Instrumentation.ActivityResult>() {
-                        @Override
-                        public void onActivityResult(Instrumentation.ActivityResult result) {
-                            if (result.getResultCode() == Activity.RESULT_OK) {
-                                Intent data = result.getData();
-                                Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
-                                imgView.setImageBitmap(imgbitmap);
-                            }
-                            else if(result.getResultCode() == Activity.RESULT_CANCELED)
-                                Log.i(TAG, "User refused to capture a picture.");
-                        }
-                    } );
-
-            private void dispatchTakePictureIntent() {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    myPictureTakerLauncher.launch(takePictureIntent);
-                }
-            }
-        });
+        // set default value email for email field, coming from MainActivity
+        Intent fromMain = getIntent();
+        emailField.setText(fromMain.getStringExtra("EMAIL"));
 
 
-*/
+        imageButton.setOnClickListener(click -> {
+                    this.dispatchTakePictureIntent();
+                });
 
 
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+             myPictureTakerLauncher.launch(takePictureIntent);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG , "In function: onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG , "In function: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "In function: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG , "In function: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG , "In function: onDestroy");
     }
 }
